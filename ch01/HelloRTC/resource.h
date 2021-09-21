6cfde8e54ffd318e523deb400cbb28e38979843f
davidapp <davidmails@126.com>
2021-09-21 21:49:16 +0800

    update icon

--- ch01/HelloRTC/resource.h
+++ ch01/HelloRTC/resource.h
@@ -4,6 +4,12 @@
 //
 #define IDD_MAINDIALOG                  101
 #define IDI_ICON1                       105
+#define IDC_BUTTON1                     1001
+#define IDC_EDIT1                       1002
+#define IDC_EDIT2                       1003
+#define IDC_BUTTON2                     1004
+#define IDC_EDIT3                       1005
+#define IDC_BUTTON3                     1006
 
 // Next default values for new objects
 // 
@@ -11,7 +17,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        106
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_CONTROL_VALUE         1007
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
