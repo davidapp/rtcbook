8adfb73e8bb2e74d9b9c17609dcdddc04adf44a7
davidapp <davidmails@126.com>
2021-10-14 10:36:19 +0800

    update

---
+++ ch03/02_PCMCapture/main.cpp
@@ -0,0 +1,40 @@
+#include "atl.h"
+#include "atldlgs.h"
+#include "resource.h"
+#include "maindlg.h"
+
+CAppModule _Module;
+
+int Run(LPTSTR /*lpCmdLine*/ = NULL, int nCmdShow = SW_SHOWDEFAULT)
+{
+    CMessageLoop theLoop;
+    _Module.AddMessageLoop(&theLoop);
+
+    CMainDlg dlgMain;
+
+    if (dlgMain.Create(NULL) == NULL)
+    {
+        ATLTRACE(_T("Main dialog creation failed!\n"));
+        return 0;
+    }
+
+    dlgMain.ShowWindow(nCmdShow);
+
+    int nRet = theLoop.Run();
+
+    _Module.RemoveMessageLoop();
+    return nRet;
+}
+
+int WINAPI _tWinMain(HINSTANCE hInstance, HINSTANCE /*hPrevInstance*/, LPTSTR lpCmdLine, int nCmdShow)
+{
+    ::InitCommonControls();
+
+    _Module.Init(NULL, hInstance);
+
+    int nRet = Run(lpCmdLine, nCmdShow);
+
+    _Module.Term();
+
+    return nRet;
+}
