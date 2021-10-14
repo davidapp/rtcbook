4c43f30f8b07ff9c8083b0e372401b172b12c6e2
davidapp <davidmails@126.com>
2021-10-15 04:34:32 +0800

    update

--- ch01/01_BufferViewer/resource.h
+++ ch01/01_BufferViewer/resource.h
@@ -3,6 +3,7 @@
 // Used by BufferViewer.rc
 //
 #define IDD_MAINDIALOG                  101
+#define IDI_ICON1                       103
 #define IDC_HEXEDIT                     1001
 #define IDC_BASEEDIT                    1002
 #define IDC_TOBASE64                    1003
@@ -12,14 +13,13 @@
 #define IDC_TOLINE                      1010
 #define IDC_TOLIST                      1011
 #define IDC_OPENFILE                    1012
-#define IDC_BUTTON1                     1013
 #define IDC_RANDOM100                   1013
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_RESOURCE_VALUE        104
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1014
 #define _APS_NEXT_SYMED_VALUE           101
