4a37c14675eed33fda2fe2b9addf38708ecd9643
davidapp <bmw.dai@gmail.com>
2022-11-25 16:44:47 +0800

    add xp

---
+++ Common/Base/DXP.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+
+#include "DTypes.h"
+#include <string>
+
+class DXP
+{
+public:
+    static DCStr GetOSName();
+    static DVoid Print(std::string str);
+
+    D_DISALLOW_ALL_DEFAULT(DXP)
+};
