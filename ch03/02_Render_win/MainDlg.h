6aa7ad809fb7b9beeeda39ebc7ef8b5dd7d80730
daiwei.lib <daiwei.lib@bytedance.com>
2023-02-17 17:56:57 +0800

    update ddraw

--- ch03/02_Render_win/MainDlg.h
+++ ch03/02_Render_win/MainDlg.h
@@ -12,6 +12,49 @@
 #include "Video/DVideoI420.h"
 #include "Video/DVideoFormat.h"
 
+#include <ddraw.h>
+#pragma comment(lib, "ddraw.lib")
+#pragma comment(lib, "dxguid.lib")
+
+LPDIRECTDRAW7        g_pDD = NULL;        // DirectDraw object
+LPDIRECTDRAWSURFACE7 g_pDDSFront = NULL;  // DirectDraw fronbuffer surface
+LPDIRECTDRAWSURFACE7 g_pDDSBack = NULL;   // DirectDraw backbuffer surface
+
+// https://www.codeproject.com/Articles/2370/Introduction-to-DirectDraw-and-Surface-Blitting
+int InitDirectDraw(HWND hwnd)
+{
+    DirectDrawCreateEx(NULL, (VOID**)&g_pDD, IID_IDirectDraw7, NULL);
+    g_pDD->SetCooperativeLevel(hwnd, DDSCL_EXCLUSIVE | DDSCL_FULLSCREEN);
+    g_pDD->SetDisplayMode(640, 480, 16, 0, 0);
+
+    DDSURFACEDESC2 ddsd;
+    ZeroMemory(&ddsd, sizeof(ddsd));
+    ddsd.dwSize = sizeof(ddsd);
+    ddsd.dwFlags = DDSD_CAPS | DDSD_BACKBUFFERCOUNT;
+    ddsd.ddsCaps.dwCaps = DDSCAPS_PRIMARYSURFACE | DDSCAPS_FLIP | DDSCAPS_COMPLEX;
+    ddsd.dwBackBufferCount = 1;
+    g_pDD->CreateSurface(&ddsd, &g_pDDSFront, NULL);
+
+    DDSCAPS2 ddscaps;
+    ZeroMemory(&ddscaps, sizeof(ddscaps));
+    ddscaps.dwCaps = DDSCAPS_BACKBUFFER;
+    g_pDDSFront->GetAttachedSurface(&ddscaps, &g_pDDSBack);
+
+    return 0;
+}
+
+void CleanUp()
+{
+    if (g_pDDSBack)
+        g_pDDSBack->Release();
+
+    if (g_pDDSFront)
+        g_pDDSFront->Release();
+
+    if (g_pDD)
+        g_pDD->Release();
+}
+
 class CMainDlg : public CDialogImpl<CMainDlg>, public CMessageFilter
 {
 public:
@@ -30,6 +73,7 @@ public:
         MESSAGE_HANDLER(WM_INITDIALOG, OnInitDialog)
         COMMAND_RANGE_HANDLER(IDOK, IDNO, OnCloseCmd)
         COMMAND_ID_HANDLER(IDC_GDI, OnGDI)
+        COMMAND_ID_HANDLER(IDC_DBUFFER, OnDoubleBuffer)
         COMMAND_ID_HANDLER(IDC_OPENGL, OnOpenGL)
         COMMAND_ID_HANDLER(IDC_DIRECTDRAW, OnDirectDraw)
     END_MSG_MAP()
@@ -44,6 +88,8 @@ public:
         m_destRect.left = 0;
         m_destRect.bottom = 300;
         m_destRect.right = 300;
+
+        InitDirectDraw(m_hWnd);
         return TRUE;
     }
 
@@ -58,33 +104,44 @@ public:
     LRESULT OnGDI(WORD wNotifyCode, WORD wID, HWND hWndCtl, BOOL& bHandled)
     {
         CDC dc = ::GetDC(m_hWnd);
-
         DVideoFrame i420frame_e = DVideoI420::Scale(m_frame, m_destRect.Width(), m_destRect.Height(), kFilterBox);
         DVideoFrame frameRaw = DVideoFormat::I420ToRAW(i420frame_e);
         DRect src = DRect(0, 0, i420frame_e.GetWidth(), i420frame_e.GetHeight());
         DBITMAPINFOHEADER* pHrd = frameRaw.NewBMPInfoHeader();
+
         HBRUSH brush1 = ::CreateSolidBrush(RGB(0, 0, 255));
-        RECT logical_rect1 = { 0, 0, m_destRect.Width(), m_destRect.Height() };
+        RECT logical_rect1 = { 0, 0, m_destRect.Width(), m_destRect.Height()-1 };
         dc.FillRect(&logical_rect1, brush1);
         ::DeleteObject(brush1);
+        
         dc.StretchDIBits(0, 0, m_destRect.Width(), m_destRect.Height(),
             0, src.Height(), src.Width(), -src.Height(), frameRaw.GetBuf(),
             (const BITMAPINFO*)pHrd, DIB_RGB_COLORS, SRCCOPY);
+        
+        delete pHrd;
         return 0;
-        // Double Buffer
+    }
+
+    LRESULT OnDoubleBuffer(WORD wNotifyCode, WORD wID, HWND hWndCtl, BOOL& bHandled)
+    {
+        CDC dc = ::GetDC(m_hWnd);
+        DVideoFrame i420frame_e = DVideoI420::Scale(m_frame, m_destRect.Width(), m_destRect.Height(), kFilterBox);
+        DVideoFrame frameRaw = DVideoFormat::I420ToRAW(i420frame_e);
+        DRect src = DRect(0, 0, i420frame_e.GetWidth(), i420frame_e.GetHeight());
+        DBITMAPINFOHEADER* pHrd = frameRaw.NewBMPInfoHeader();
+
         CDC dc_mem;
         dc_mem.CreateCompatibleDC(dc.m_hDC);
-        //dc_mem.SetStretchBltMode(HALFTONE);
 
         HBITMAP bmp_mem = ::CreateCompatibleBitmap(dc.m_hDC, m_destRect.Width(), m_destRect.Height());
         HGDIOBJ bmp_old = ::SelectObject(dc_mem.m_hDC, bmp_mem);
         HBRUSH brush = ::CreateSolidBrush(RGB(0, 0, 255));
-        RECT logical_rect = { 0, 0, m_destRect.Width(), m_destRect.Height() };
+        RECT logical_rect = { 0, 0, m_destRect.Width(), m_destRect.Height() - 1 };
         ::FillRect(dc_mem, &logical_rect, brush);
         ::DeleteObject(brush);
 
         dc_mem.StretchDIBits(0, 0, m_destRect.Width(), m_destRect.Height(),
-            0, src.Height(), src.Width(), src.Height(), frameRaw.GetBuf(),
+            0, src.Height(), src.Width(), -src.Height(), frameRaw.GetBuf(),
             (const BITMAPINFO*)pHrd, DIB_RGB_COLORS, SRCCOPY);
 
         dc.BitBlt(m_destRect.left, m_destRect.top, m_destRect.Width(), m_destRect.Height(), dc_mem, 0, 0, SRCCOPY);
@@ -104,9 +161,64 @@ public:
 
     LRESULT OnDirectDraw(WORD wNotifyCode, WORD wID, HWND hWndCtl, BOOL& bHandled)
     {
+        m_pSurface->Restore();
+
+        DDSURFACEDESC2 ddsd;
+        ddsd.dwSize = sizeof(ddsd);
+        ddsd.dwFlags = DDSD_HEIGHT | DDSD_WIDTH;
+        m_pSurface->GetSurfaceDesc(&ddsd);
+
+
         return 0;
+    }
+
+    DBool CreateSurface(LPDIRECTDRAW7 hDD, int nWidth, int nHeight, COLORREF dwColorKey)
+    {
+        DDSURFACEDESC2    ddsd;
+        ZeroMemory(&ddsd, sizeof(ddsd));
+        ddsd.dwSize = sizeof(ddsd);
+        ddsd.dwFlags = DDSD_CAPS | DDSD_WIDTH | DDSD_HEIGHT;
+        ddsd.ddsCaps.dwCaps = DDSCAPS_OFFSCREENPLAIN | DDSCAPS_VIDEOMEMORY;
+        ddsd.dwWidth = nWidth;
+        ddsd.dwHeight = nHeight;
+
+        HRESULT hRet;
+        hDD->CreateSurface(&ddsd, &m_pSurface, NULL);
+        if (hRet != DD_OK)
+        {
+            if (hRet == DDERR_OUTOFVIDEOMEMORY)
+            {
+                ddsd.ddsCaps.dwCaps = DDSCAPS_OFFSCREENPLAIN |
+                    DDSCAPS_SYSTEMMEMORY;
+
+                hRet = hDD->CreateSurface(&ddsd, &m_pSurface, NULL);
+            }
+
+            if (hRet != DD_OK)
+            {
+                return FALSE;
+            }
+        }
+
+        DDCOLORKEY ddck;
+        if ((int)dwColorKey != -1)
+        {
+            ddck.dwColorSpaceLowValue = dwColorKey;
+            ddck.dwColorSpaceHighValue = 0;
+            m_pSurface->SetColorKey(DDCKEY_SRCBLT, &ddck);
+        }
+
+        m_ColorKey = dwColorKey;
+        m_Width = nWidth;
+        m_Height = nHeight;
     }
 
     DVideoFrame m_frame;
     DRect m_destRect;
+
+    // DDraw
+    COLORREF m_ColorKey;
+    UINT m_Height;
+    UINT m_Width;
+    LPDIRECTDRAWSURFACE7 m_pSurface;
 };
--- ch03/02_Render_win/resource.h
+++ ch03/02_Render_win/resource.h
@@ -6,6 +6,8 @@
 #define IDC_GDI                         1001
 #define IDC_OPENGL                      1002
 #define IDC_DIRECTDRAW                  1003
+#define IDC_BUTTON1                     1004
+#define IDC_DBUFFER                     1004
 
 // Next default values for new objects
 // 
@@ -13,7 +15,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        103
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1004
+#define _APS_NEXT_CONTROL_VALUE         1005
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
