74f075cc95630760cd1698f5936f11073607c02f
daiwei.lib <daiwei.lib@bytedance.com>
2023-02-22 18:14:23 +0800

    update gl

--- ch03/02_Render_win/resource.h
+++ ch03/02_Render_win/resource.h
@@ -9,6 +9,8 @@
 #define IDC_BUTTON1                     1004
 #define IDC_DBUFFER                     1004
 #define IDC_SHOW                        1005
+#define IDC_BUTTON2                     1006
+#define IDC_OPENGL2                     1006
 
 // Next default values for new objects
 // 
@@ -16,7 +18,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        103
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1006
+#define _APS_NEXT_CONTROL_VALUE         1007
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
